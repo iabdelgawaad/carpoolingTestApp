9c2b3ad884202650c52a46d47e5a4c36d26b5d39
Ibrahim AbdelGawad <iabdelgawaad@gmail.com>
2017-10-16 00:51:45 +0200

    added simple unit test using mockito

--- app/src/test/java/com/example/ibrahimabdelgawad/carpoolingtestapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/ibrahimabdelgawad/carpoolingtestapp/ExampleUnitTest.java
@@ -1,8 +1,15 @@
 package com.example.ibrahimabdelgawad.carpoolingtestapp;
 
-import org.junit.Test;
+import android.content.Context;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +17,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    private static final String FAKE_STRING = "loading";
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
     }
+    
+    @Mock
+    Context mMockContext;
+
+    @Test
+    public void readStringFromContext_LocalizedString() {
+        // Given a mocked Context injected into the object under test...
+        when(mMockContext.getString(R.string.loading_text))
+                .thenReturn(FAKE_STRING);
+
+        // ...then the result should be the expected one.
+        assertThat(mMockContext.getString(R.string.loading_text), is(FAKE_STRING));
+    }
+
 }
